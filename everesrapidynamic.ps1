a85005d978eb8fb62da26a3c9d3f923635f5744e
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2023-09-25 14:56:11 +0530

    Update everesrapidynamic.ps1

--- everesrapidynamic.ps1
+++ everesrapidynamic.ps1
@@ -10,23 +10,33 @@ $config = Get-Content -Path $configFile | ForEach-Object {
     }
 }
 
-# Access values from the configuration object
-$subscriptionId = $config | Where-Object { $_.Key -eq "SubscriptionId" } | Select-Object -ExpandProperty Value
-$resourceGroupName = $config | Where-Object { $_.Key -eq "ResourceGroupName" } | Select-Object -ExpandProperty Value
-$apiName = $config | Where-Object { $_.Key -eq "ApiName" } | Select-Object -ExpandProperty Value
-$specificationUrl = $config | Where-Object { $_.Key -eq "SpecificationUrl" } | Select-Object -ExpandProperty Value
-$apiId = $config | Where-Object { $_.Key -eq "ApiId" } | Select-Object -ExpandProperty Value
-$apimName = $config | Where-Object { $_.Key -eq "ApimName" } | Select-Object -ExpandProperty Value
-$apiPolicyConfigFilePath = $config | Where-Object { $_.Key -eq "ApiPolicyConfigFilePath" } | Select-Object -ExpandProperty Value
-$apiVisibility = $config | Where-Object { $_.Key -eq "ApiVisibility" } | Select-Object -ExpandProperty Value
-$postmanCollectionFilePath = $config | Where-Object { $_.Key -eq "PostmanCollectionFilePath" } | Select-Object -ExpandProperty Value
+# Access the OAS file path from the configuration object
+$oasFilePath = $config | Where-Object { $_.Key -eq "OasFilePath" } | Select-Object -ExpandProperty Value
 
-# Authenticate with your Azure account
+# Authenticate with your Azure account and add debugging output
+Write-Output "Authenticating with Azure..."
 az login --use-device-code
 
+# Check if authentication was successful
+if ($?) {
+    Write-Output "Azure authentication successful."
+} else {
+    Write-Error "Azure authentication failed."
+    exit 1
+}
+
 # Step 1: API Creation and Validation
-# Create API in APIM using validated OAS specification
-az apim api import --resource-group $resourceGroupName --service-name $apimName --path "/$apiName" --api-id $apiId --specification-url $specificationUrl --specification-format OpenApiJson
+# Create API in APIM using the OAS file path from your configuration
+Write-Output "Importing API from OAS file..."
+az apim api import --path "/$apiName" --resource-group $resourceGroupName --service-name $apimName --specification-format OpenApiJson --specification-path $oasFilePath --api-id $apiId
+
+# Check the result of API import
+if ($?) {
+    Write-Output "API import successful."
+} else {
+    Write-Error "API import failed."
+    exit 1
+}
 
 # Step 2: Azure API Management Setup
 # If APIM instance does not exist, create it
