ec4ab3ba160fea187dda315a32f98ee326143c0b
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2023-09-29 16:57:34 +0530

    Update everesrapidynamic.ps1

--- everesrapidynamic.ps1
+++ everesrapidynamic.ps1
@@ -57,12 +57,20 @@ $simplifiedVersion = $oasVersion -replace '\.', ''
 if ($oasVersion -match '^\d+\.\d+\.\d+$') {
     $simplifiedVersion = "v$($oasVersion -replace '\.', '')"  # Remove dots from the version
     
-    # Add a unique suffix to the API name based on the version
+    # Modify the API name to include the version
     $newApiName = "${apiName}-${simplifiedVersion}"
-    Write-Output "Creating a new API for version $simplifiedVersion with name $newApiName"
     
-    # Import the new API
-    $api = Import-AzApiManagementApi -Context $apimContext -ApiId $newApiName -Path "/$newApiName" -SpecificationPath $oasFilePath -SpecificationFormat OpenApiJson
+    # Check if an API with the same name already exists
+    $existingApi = Get-AzApiManagementApi -Context $apimContext -ApiId $newApiName -ResourceGroupName $resourceGroupName
+    
+    if ($existingApi -eq $null) {
+        # If the API doesn't exist, create it
+        Write-Output "Creating a new API for version $simplifiedVersion with name $newApiName"
+        $api = Import-AzApiManagementApi -Context $apimContext -ApiId $newApiName -Path "/$newApiName" -SpecificationPath $oasFilePath -SpecificationFormat OpenApiJson
+    } else {
+        # If the API already exists, consider handling this case, e.g., creating a new revision
+        Write-Output "API with name $newApiName already exists. You may want to create a new revision or handle this case accordingly."
+    }
 } else {
     Write-Error "Invalid version format: $oasVersion"
     exit 1
