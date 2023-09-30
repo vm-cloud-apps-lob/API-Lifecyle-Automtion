ae17411bee4e06b0c8271597444446b8a8d78853
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2023-09-30 14:49:10 +0530

    Update everesrapidynamic.ps1

--- everesrapidynamic.ps1
+++ everesrapidynamic.ps1
@@ -43,21 +43,27 @@ function Get-YamlVersion($yamlContent) {
 $oasContent = Get-Content -Path $oasFilePath -Raw
 $oasVersion = Get-YamlVersion -yamlContent $oasContent
 
+# Debugging output
+Write-Output "OAS Version: $oasVersion"
+
 # Check if the version follows the pattern of x.y.z (e.g., 1.0.0, 2.0.0, 1.0.1, etc.)
 if ($oasVersion -match '^\d+\.\d+\.\d+$') {
     $majorVersion = [int]($oasVersion.Split('.')[0])
     $minorVersion = [int]($oasVersion.Split('.')[1])
 
-    # Check if it's a major version change (minor version is zero)
+    # Debugging output
+    Write-Output "Major Version: $majorVersion"
+    Write-Output "Minor Version: $minorVersion"
+
     if ($minorVersion -eq 0) {
+        # If minor version is 0, it's a major version change, create a new API
         Write-Output "Creating a new API for version $oasVersion"
         $api = Import-AzApiManagementApi -Context $apimContext -ApiId $apiName -Path "/$apiName-v$majorVersion" -SpecificationPath $oasFilePath -SpecificationFormat OpenApiJson
-    }
-    # Check if it's a revision (minor version is greater than zero)
-    elseif ($minorVersion -gt 0) {
+    } else {
+        # If minor version is greater than 0, it's a revision
         Write-Output "Creating a revision for API version $oasVersion"
         $apiRevision = $oasVersion -replace '\.', '-'
-        $api = New-AzApiManagementApiRevision -Context $apimContext -ApiId $apiName -ApiRevision $apiRevision
+        $api = New-AzApiManagementApiRevision -Context $apimContext -ApiId $apiId -ApiRevision $apiRevision
     }
 } else {
     Write-Error "Invalid version format: $oasVersion"
