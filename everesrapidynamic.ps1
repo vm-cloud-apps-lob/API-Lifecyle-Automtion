121d8c98c016854a884dd6837ee8f038f5b86790
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2023-10-09 19:10:30 +0530

    Update everesrapidynamic.ps1

--- everesrapidynamic.ps1
+++ everesrapidynamic.ps1
@@ -14,8 +14,10 @@ $config = Get-Content -Path $configFile | ForEach-Object {
 $subscriptionId = $config | Where-Object { $_.Key -eq "SubscriptionId" } | Select-Object -ExpandProperty Value
 $resourceGroupName = $config | Where-Object { $_.Key -eq "ResourceGroupName" } | Select-Object -ExpandProperty Value
 $apiName = $config | Where-Object { $_.Key -eq "ApiName" } | Select-Object -ExpandProperty Value
+$apiId = $config | Where-Object { $_.Key -eq "ApiId" } | Select-Object -ExpandProperty Value
 $apimName = $config | Where-Object { $_.Key -eq "ApimName" } | Select-Object -ExpandProperty Value
 $apiPolicyConfigFilePath = $config | Where-Object { $_.Key -eq "ApiPolicyConfigFilePath" } | Select-Object -ExpandProperty Value
+$apiVisibility = $config | Where-Object { $_.Key -eq "ApiVisibility" } | Select-Object -ExpandProperty Value
 $postmanCollectionFilePath = $config | Where-Object { $_.Key -eq "PostmanCollectionFilePath" } | Select-Object -ExpandProperty Value
 
 # Specify the path to your OAS file in the repository
@@ -25,11 +27,16 @@ $oasFilePath = "$env:GITHUB_WORKSPACE\openapi.yaml"
 Connect-AzAccount -UseDeviceAuthentication
 
 # Check if authentication was successful
-if (-not $?) {
+if ($?) {
+    Write-Output "Azure authentication successful."
+} else {
     Write-Error "Azure authentication failed."
     exit 1
 }
 
+# Step 1: API Creation and Validation
+# Create API in APIM using the OAS file path from your configuration
+Write-Output "Importing API from OAS file..."
 # Create the API Management context
 $apimContext = New-AzApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimName
 
@@ -44,40 +51,37 @@ function Get-YamlVersion($yamlContent) {
 $oasContent = Get-Content -Path $oasFilePath -Raw
 $oasVersion = Get-YamlVersion -yamlContent $oasContent
 
-# Check if the version follows the pattern of x.y.z (e.g., 1.0.0, 2.0.0, 1.0.1, etc.)
-if ($oasVersion -match '^\d+\.\d+\.\d+$') {
-    $majorVersion = [int]($oasVersion.Split('.')[0])
-    $minorVersion = [int]($oasVersion.Split('.')[1])
+# Replace dots with hyphens in the version for the API revision
+$apiRevision = $oasVersion -replace '\.', '-'
 
-    # Check if it's a major version change (minor version is zero)
-    if ($minorVersion -eq 0) {
-        Write-Output "Creating a new API for version $oasVersion"
-        
-        # Construct the API path with a valid identifier (replace dots with hyphens)
-        $apiPath = "/$apiName-v$majorVersion"
-        $apiRevision = $oasVersion -replace '\.', '-'
+# Import API using the local file path and specify the -ApiRevision parameter
+$api = Import-AzApiManagementApi -Context $apimContext -ApiId $apiId -Path "/$apiName" -SpecificationPath $oasFilePath -SpecificationFormat OpenApiJson -ApiRevision $apiRevision
 
-        # Import API using the local file path and specify the -ApiRevision parameter
-        $api = Import-AzApiManagementApi -Context $apimContext -ApiId $apiPath -Path $apiPath -SpecificationPath $oasFilePath -SpecificationFormat OpenApiJson -ApiRevision $apiRevision
-    }
-    # Check if it's a revision (minor version is greater than zero)
-    elseif ($minorVersion -gt 0) {
-        Write-Output "Creating a revision for API version $oasVersion"
-        
-        # Construct the API path with a valid identifier (replace dots with hyphens)
-        $apiPath = "/$apiName-v$majorVersion"
-        $apiRevision = $oasVersion -replace '\.', '-'
-
-        # Import API using the local file path and specify the -ApiRevision parameter
-        $api = New-AzApiManagementApiRevision -Context $apimContext -ApiId $apiPath -ApiRevision $apiRevision
-    }
+# Check the result of API import
+if ($?) {
+    Write-Output "API import successful. Detected API revision: $($api.ApiRevision)"
 } else {
-    Write-Error "Invalid version format: $oasVersion"
+    Write-Error "API import failed."
     exit 1
 }
 
-# Set policies using Set-AzApiManagementPolicy
+# Step 2: Azure API Management Setup
+# If APIM instance does not exist, create it
+$existingApim = Get-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimName -ErrorAction SilentlyContinue
+if ($null -eq $existingApim) {
+    New-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimName -PublisherEmail "vamsi.sapireddy@valuemomentum.com" -PublisherName "Vamsi"
+}
+
+# Step 3: Set API Management context
+$apimContext = New-AzApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimName
+
+# Read the policies content from your policy config file
 $apiPolicies = Get-Content -Path $apiPolicyConfigFilePath -Raw
-Set-AzApiManagementPolicy -Context $apimContext -ApiId $apiPath -Policy $apiPolicies
+
+# Set policies using Set-AzApiManagementPolicy
+Set-AzApiManagementPolicy -Context $apimContext -ApiId $apiId -Policy $apiPolicies
+
+# Associate the API with the existing product "Unlimited"
+Add-AzApiManagementApiToProduct -Context $apimContext -ApiId $apiId -ProductId "Unlimited"
 
 Write-Output "Script execution completed."
